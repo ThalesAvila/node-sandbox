0b69809825af55cad6c891c9352e4aaf472e3bc0
Thales <thalespizza@gmail.com>
2017-01-31 03:01:41 -0200

    Exemplos de Web Servers

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 var http = require('http');
+var fs   = require('fs');
 
 http.createServer(function(req, res) {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end("Hello world!\n");
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    html = fs.createReadStream(__dirname + '/index.htm', 'utf-8').pipe(res);
+    
 }).listen(1337, '127.0.0.1');
---
+++ app2.js
@@ -0,0 +1,14 @@
+var http = require('http');
+var fs   = require('fs');
+
+http.createServer(function(req, res) {
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+    
+    var obj = {
+        firstname: 'John',
+        lastname: 'Doe'
+    }
+    //Objeto serializado
+    res.end(JSON.stringify(obj));
+    
+}).listen(1337, '127.0.0.1');
